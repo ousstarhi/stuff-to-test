d0c95db953e0990c4ab936ea2b3acc84b41d40b2
Oussama <ousstarhi.web@gmail.com>
2020-05-17 18:08:43 +0000

    custom toUpperCase method

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ document.getElementById('app').innerHTML = `
 `;
 
 //in C we can deal with strings as an array of chars, unlike JavaScript where the string is immutable.
+const myString = 'This is my name: Oussama!';
+const myArray = myString.split('');
+for (let i = 0; i < myArray.length; i += 1) {
+	if (myArray[i].charCodeAt(0) >= 97 && myArray[i].charCodeAt(0) <= 122) {
+		myArray[i] = String.fromCharCode(myArray[i].charCodeAt(0) - 32);
+	}
+}
+console.log(myArray.join(''));
