7a154495e4a371dd2f9db42706c3de2c373f5141
Oussama <ousstarhi.web@gmail.com>
2020-05-17 17:37:07 +0000

    Strings in C and JavaScript

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import './styles.css';
 document.getElementById('app').innerHTML = `
 <h1>Hello Vanilla!</h1>
 `;
+
+//in C we can deal with strings as an array of chars, unlike JavaScript where the string is immutable.
